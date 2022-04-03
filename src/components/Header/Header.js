631f12fb70095c92970fbee4246f104542df0c40
Prohor722 <prohor.b722@gmail.com>
2022-04-04 03:13:24 +0600

    navbar added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
     return (
-        <div>
+        <div className='nav'>
             <Link to="/">Home</Link>
-            {/* <Link to="/"></Link> */}
+            <Link to="/review">Review</Link>
+            <Link to="/dashboard">Dashboard</Link>
+            <Link to="/blog">Blog</Link>
+            <Link to="/about">About</Link>
         </div>
     );
 };
